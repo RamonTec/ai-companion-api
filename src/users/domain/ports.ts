574b0017b4db06de96afd733be21fc7b5fc8e4cc
Elias Ramon <eestrabao46@gmail.com>
2026-04-22 21:47:06 -0400

    feat: removed dependenci with dto, now using class domain user

--- src/users/domain/ports.ts
+++ src/users/domain/ports.ts
@@ -1,8 +1,7 @@
 import { User } from "./entities/user.entity.js";
-import { UserRegisterDto } from "../application/dtos/user-register.dto.js";
 
 export abstract class IUserRepository {
-    abstract register(userRegisterDto: UserRegisterDto): Promise<User>;
+    abstract register(user: User): Promise<User>;
     abstract findByEmail(email: string): Promise<User | null>;
     abstract findByNickName(nickName: string): Promise<User | null>;
     abstract findUserById(id: string): Promise<User | null>;
