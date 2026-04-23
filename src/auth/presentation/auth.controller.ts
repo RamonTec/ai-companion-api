4b0758a337b6a63ba9e4c9dca4aa94e726f55e29
Elias Ramon <eestrabao46@gmail.com>
2026-04-22 21:46:25 -0400

    feat: added new endpoint to login users

--- src/auth/presentation/auth.controller.ts
+++ src/auth/presentation/auth.controller.ts
@@ -1,14 +1,24 @@
 import { Body, Controller, Post, Req } from "@nestjs/common";
-import { UserRegisterDto } from "../application/dtos/user-register.dto.js";
-import { UserRegisterUseCase } from "../application/use-cases/user-resgister.use-case.js";
+import { LoginDto } from "../application/dtos/login.dto.js";
+import { LoginUseCase } from "../application/use-cases/login.use-case.js";
+import { UserRegisterUseCase } from "@/users/application/use-cases/user-resgister.use-case.js";
+import { UserRegisterDto } from "@/users/application/dtos/user-register.dto.js";
 
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly userRegisterUseCase: UserRegisterUseCase) { }
+    constructor(
+        private readonly userRegisterUseCase: UserRegisterUseCase,
+        private readonly userLoginUseCase: LoginUseCase
+    ) { }
 
-    @Post()
+    @Post('register')
     register(@Body() dto: UserRegisterDto) {
         return this.userRegisterUseCase.execute(dto);
+    }
+
+    @Post('login')
+    login(@Body() dto: LoginDto) {
+        return this.userLoginUseCase.execute(dto);
     }
 }
