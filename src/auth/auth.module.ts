d2ff023fd3400af6c3674ac58e85289e9fb116ba
Elias Ramon <eestrabao46@gmail.com>
2026-04-26 11:32:37 -0400

    feat: imported user module and providers for use cases and impl and providers DIP

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,26 @@
 import { Module } from "@nestjs/common";
-import { UserRegisterUseCase } from "./application/use-cases/user-resgister.use-case.js";
-import { IAuthRepository } from "./domain/ports.js";
 import { AuthController } from "./presentation/auth.controller.js";
-
+import { IAuthUserProvider, IPasswordHasher, ITokenProvider } from "./domain/ports.js";
+import { TokenProviderImpl } from "./infrastructure/adapters/token-provider.impl.js";
+import { PasswordHasherImpl } from "./infrastructure/adapters/password-hasher.impl.js";
+import { AuthUserProviderImpl } from "./infrastructure/adapters/auth-user-provider.impl.js";
+import { LoginUseCase } from "./application/use-cases/login.use-case.js";
+import { ResetPasswordUseCase } from "./application/use-cases/reset-password.use-case.js";
+import { RecoveryPasswordUseCase } from "./application/use-cases/recovery-password.use-case.js";
+import { UserModule } from "@/users/user.module.js";
+import { UserRegisterUseCase } from "@/users/application/use-cases/user-resgister.use-case.js";
 
 @Module({
+    imports: [UserModule], // Import UserModule if needed for UserRegisterUseCase and IUserRepository
     controllers: [AuthController],
     providers: [
-        {
-            provide: IAuthRepository,
-            useClass: AuthRepositoryImpl,
-        }
+        { provide: ITokenProvider, useClass: TokenProviderImpl },
+        { provide: IPasswordHasher, useClass: PasswordHasherImpl },
+        { provide: IAuthUserProvider, useClass: AuthUserProviderImpl },
+        LoginUseCase,
+        ResetPasswordUseCase,
+        RecoveryPasswordUseCase,
     ],
-    exports: [UserRegisterUseCase],
+    exports: [],
 })
 export class AuthModule { }
