819112c77288770552ef583c82bfc5e0a935f7fc
Elias Ramon <eestrabao46@gmail.com>
2026-04-30 23:33:19 -0400

    feat: new dto

---
+++ src/ia-models/application/dtos/update-ia-model.dto.ts
@@ -0,0 +1,16 @@
+
+export class UpdateIaModelDto {
+    name: string;
+    description: string;
+    category: string;
+    personality: string;
+    provider: string;
+    providerModelId: string;
+    basePrompt: string;
+    temperature: number;
+    topP: number;
+    voiceId: string;
+    knowledgeBaseId: string;
+    requiredTier: string;
+    maxTokens: number;
+}
