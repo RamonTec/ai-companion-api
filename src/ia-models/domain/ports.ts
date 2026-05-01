304c0e8a02b0258a27e7b22f41b6ee50d4dc5bdf
Elias Ramon <eestrabao46@gmail.com>
2026-04-30 23:33:52 -0400

    feat: defined new contracts

--- src/ia-models/domain/ports.ts
+++ src/ia-models/domain/ports.ts
@@ -1,5 +1,7 @@
-import { IaModel, IaModelCreate } from "./ia-models.js";
+import { IaModel, IaModelCreate, IaModelUpdate } from "./ia-models.js";
 
 export abstract class IaModelRepository {
     abstract register(data: IaModelCreate): Promise<IaModel>;
+    abstract update(id: string, data: IaModelUpdate): Promise<IaModel>;
+    abstract findById(id: string): Promise<IaModel>;
 }
