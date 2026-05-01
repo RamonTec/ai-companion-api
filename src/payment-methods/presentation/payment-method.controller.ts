b07e826951e18b29e98725a36dfdf56e99e3b7e4
Elias Ramon <eestrabao46@gmail.com>
2026-05-01 14:30:27 -0400

    feat: added auth guard to protect resources

--- src/payment-methods/presentation/payment-method.controller.ts
+++ src/payment-methods/presentation/payment-method.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { RegisterPaymentMethodUseCase } from "../application/use-cases/register-payment-method.use-case.js";
 import { RegisterPaymentMethodDto } from "../application/dtos/register-payment-method.dto.js";
 import { PaymentMethod } from "../domain/entities/payment-method.entity.js";
+import { AuthGuard } from "@/common/guards/auth.guard.js";
 
 @Controller('payment-methods')
 export class PaymentMethodController {
     constructor(private readonly paymentMethodUseCase: RegisterPaymentMethodUseCase) { }
 
+    @UseGuards(AuthGuard)
     @Post('register')
     async register(@Body() registerPaymentMethodDto: RegisterPaymentMethodDto): Promise<PaymentMethod> {
         return await this.paymentMethodUseCase.execute(registerPaymentMethodDto);
