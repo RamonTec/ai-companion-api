7b3b3ed5554fbe84b58d22c608e57a3bece18190
Elias Ramon <eestrabao46@gmail.com>
2026-04-30 23:33:12 -0400

    feat: imported new use case update ai model

--- src/ia-models/ia-models.module.ts
+++ src/ia-models/ia-models.module.ts
@@ -3,12 +3,14 @@ import { RegisterIaModelUseCase } from "./application/use-cases/register-ia-mode
 import { IaModelRepository } from "./domain/ports.js";
 import { IaModelsController } from "./ia-models.controller.js";
 import { IaModelAdapterImpl } from "./infra/providers/ia-model-adapter.impl.js";
+import { UpdateIaModelUseCase } from "./application/use-cases/update-ia-model.use-case.js";
 
 @Module({
     controllers: [IaModelsController],
     providers: [
         { provide: IaModelRepository, useClass: IaModelAdapterImpl },
         RegisterIaModelUseCase,
+        UpdateIaModelUseCase,
     ],
     exports: [],
 })
