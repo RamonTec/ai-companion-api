e73a54152f741a3a12279589633e81498bfb3fda
Elias Ramon <eestrabao46@gmail.com>
2026-04-30 23:32:43 -0400

    feat: added new endpoint to update an ai model

--- src/ia-models/ia-models.controller.ts
+++ src/ia-models/ia-models.controller.ts
@@ -1,16 +1,24 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Param, Post, Put } from "@nestjs/common";
 import { RegisterIaModelDto } from "./application/dtos/register-ia-model.dto.js";
 import { RegisterIaModelUseCase } from "./application/use-cases/register-ia-model.use-case.js";
+import { UpdateIaModelUseCase } from "./application/use-cases/update-ia-model.use-case.js";
+import { UpdateIaModelDto } from "./application/dtos/update-ia-model.dto.js";
 
 @Controller('ia-models')
 export class IaModelsController {
     constructor(
         private readonly iaModelRegisterUseCase: RegisterIaModelUseCase,
+        private readonly iaModelUpdateUseCase: UpdateIaModelUseCase,
     ) { }
 
     @Post('')
     register(@Body() dto: RegisterIaModelDto) {
         return this.iaModelRegisterUseCase.execute(dto);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() dto: UpdateIaModelDto) {
+        return this.iaModelUpdateUseCase.execute(id, dto);
     }
 
 }
