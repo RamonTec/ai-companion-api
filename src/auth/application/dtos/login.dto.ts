aa2d7c50bd33769d7475a11f2b9132e1d7ef614d
Elias Ramon <eestrabao46@gmail.com>
2026-05-06 22:52:50 -0400

    feat: enhanced dto

--- src/auth/application/dtos/login.dto.ts
+++ src/auth/application/dtos/login.dto.ts
@@ -1,10 +1,13 @@
-import { IsEmail, IsString, MinLength } from "class-validator";
-
+import { IsEmail, MinLength, MaxLength, Matches } from 'class-validator';
 
 export class LoginDto {
     @IsEmail()
     email: string;
-    @IsString()
-    @MinLength(6)
+
+    @MinLength(8)
+    @MaxLength(72)
     password: string;
+
+    ipAddress?: string;
+    userAgent?: string;
 }
